f3e4e8d932d24d0b7d403b1350468f90a8481f39
Wagner Marques <wagner.marques@twavectr.com>
2025-05-12 08:37:40 -0300

    First commit

---
+++ backend/src/main/java/br/com/eicon/consulta_creditos/ConsultaCreditosApplication.java
@@ -0,0 +1,13 @@
+package br.com.eicon.consulta_creditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCreditosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/eicon/consulta_creditos/ConsultaCreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eicon.consulta_creditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
